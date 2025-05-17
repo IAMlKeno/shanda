ffb1f4bf3f4436ce0d963b82b9788d7802e7ffe0
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-05-17 15:07:19 -0300

    feat: merge react branch and add auth0 sdk

--- shanda-app/src/main.tsx
+++ shanda-app/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Auth0Provider
+  // get from dot entv `import.meta.env.VITE_API`
+    domain="dev-shandaauto.ca.auth0.com"
+    clientId="XrF1RcIzMwLDmtZNSeikLFzEL5qgeQ5l"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>,
 )
