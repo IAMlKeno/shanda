bd6a8469d95a34f24556a93f46ae3e6ac30ffd5b
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-06-02 21:02:40 -0300

    feat: add auth0 domain settings and update vite settings

--- shanda-app/src/main.tsx
+++ shanda-app/src/main.tsx
@@ -3,13 +3,19 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+const AUTH0_AUDIENCE = import.meta.env.AUTH0_AUDIENCE;
+const AUTH0_DOMAIN = import.meta.env.AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID = import.meta.env.AUTH0_CLIENT_ID;
+
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
   // get from dot entv `import.meta.env.VITE_API`
-    domain="dev-shandaauto.ca.auth0.com"
-    clientId="XrF1RcIzMwLDmtZNSeikLFzEL5qgeQ5l"
+    domain={AUTH0_DOMAIN}
+    clientId={AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: AUTH0_AUDIENCE,
+      scope: "read:current_user update:current_user_metadata"
     }}
   >
     <App />
