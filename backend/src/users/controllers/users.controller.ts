4c613233cbe7a48a225f67f589746b7c51f7f749
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-06-07 13:02:16 -0300

    feat: clean up users controller for now

--- backend/src/users/controllers/users.controller.ts
+++ backend/src/users/controllers/users.controller.ts
@@ -1,15 +1,11 @@
-import { Body, Controller, Get, HttpStatus, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { BaseController } from 'src/mvc/base/base.controller';
 import { UserHandler } from '../handler/user.handler';
 import { UserListResponse, UserRequest, UserResponse } from '../entities/user.entity';
 import { UserDto } from '../dto/user.dto';
 import { ProfileHandler } from 'src/profiles/handlers/profiles.handler';
-import { GarageOwnerDto } from 'src/profiles/dto/garage-owner/garage-owner.dto';
-import { ProviderDto } from 'src/profiles/dto/provider/provider.dto';
-import { RequesterDto } from 'src/profiles/dto/requester/requester.dto';
 import { ContactInformationHandler } from 'src/contact-information/handlers/contact-information.handler';
-import { ContactInformationDto } from 'src/contact-information/dto/contact-information.dto';
-import { ApiFoundResponse, ApiHeader, ApiNotFoundResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiFoundResponse, ApiHeader, ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
 import { ErrorResponse } from 'src/mvc/base/http/entities';
 import { JwtService } from '@nestjs/jwt';
 
@@ -35,7 +31,7 @@ export class UsersController extends BaseController<UserHandler, UserRequest, Us
     return new UserListResponse(users.map((user) => user.user), total);
   }
 
-  @ApiOperation({ summary: 'Get the successfully authenticated user and profiles.', operationId: 'loadUser'})
+  @ApiOperation({ summary: 'Get the successfully authenticated user and profiles.', operationId: 'loadUser' })
   @ApiFoundResponse({ type: UserResponse, description: 'A full user object containing references to its profiles.' })
   @ApiNotFoundResponse({ type: ErrorResponse, description: 'Failed to find user.' })
   @ApiHeader({ name: 'user-token', description: 'User auth token' })
@@ -65,7 +61,7 @@ export class UsersController extends BaseController<UserHandler, UserRequest, Us
     // probably add some interceptor that can extract user token from the request
     const token = req.headers['user-token'];
     const requester = await this.profileHandler.requesterService.getUserProfile(token);
-    const garageId = requester.requester.garageId;
+    const garageId = requester.garageId;
     const garage = await this.profileHandler.requesterService.getGarageById(garageId);
 
     return { requester, garage };
